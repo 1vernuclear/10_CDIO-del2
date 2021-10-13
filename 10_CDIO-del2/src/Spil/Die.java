4f8e9b9430f59e9d6a4180860f03f80c871056df
1vernuclear <83249678+1vernuclear@users.noreply.github.com>
2021-10-13 12:30:35 +0200

    Add files via upload
    Importeret Die-klasse fra CDIO-del1

---
+++ 10_CDIO-del2/src/Spil/Die.java
@@ -0,0 +1,42 @@
+package Spil;
+
+import java.util.Random;
+
+public class Die {
+    private Random random = new Random();
+    private int faceValue, numberOfSides;
+    /**
+     * Constructor sets amount of sides of die
+     * @param sides: int, defines number of sides
+     */
+    public Die (int sides){
+        numberOfSides = sides;
+    }
+    /**
+     * Constructor sets number of sides to 6
+     */
+    public Die() {
+        numberOfSides = 6;
+    }
+    /**
+     * Rolls die and returns integer
+     */
+    public void rollDie() {
+        faceValue = random.nextInt(numberOfSides)+1;
+    }
+    /**
+     * Sets specific value of eyes on die
+     * @param value: int, sets new value of faceValue.
+     */
+    public void setFaceValue(int value) {
+        faceValue = value;
+    }
+    /**
+     *  Return face value of die
+     */
+    public int getFaceValue(){
+        return faceValue;
+
+    }
+
+}
