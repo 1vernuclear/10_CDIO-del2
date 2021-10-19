42f5b179dc13e98db3a983b13257a97b5e6fdd97
falknielsen <90193373+falknielsen@users.noreply.github.com>
2021-10-19 14:27:18 +0200

    Initial commit af Square rod idfk

---
+++ 10_CDIO-del2/src/Spil/SquareList.java
@@ -0,0 +1,32 @@
+package Spil;
+
+public class SquareList {
+
+    public static String[][] SquareList = {
+            {"Tower", "250", "False"},
+            {"Crater", "-100", "False"},
+            {"PalaceGates", "100", "False"},
+            {"ColdDesert", "-20", "False"},
+            {"WalledCity", "180", "False"},
+            {"Monastery", "0", "False"},
+            {"BlackCave", "-70", "False"},
+            {"HutsMountain", "60", "False"},
+            {"Werewall", "-80", "True"},
+            {"Pit", "-50", "False"},
+            {"Goldmine", "650", "False"},
+    };
+
+    static int getSquareScore(int square){
+        int penge = Integer.parseInt(SquareList[square][1]);
+        return penge;
+    }
+
+    static String getSquareName(int square){
+        return SquareName.getString(SquareList[square][0]);
+    }
+
+    static boolean getExtraTurn(int square){
+        return Boolean.parseBoolean(SquareList[square][2]);
+    }
+
+}
