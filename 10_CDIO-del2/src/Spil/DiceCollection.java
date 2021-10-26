b1bd1ce84f7012a7119930d8b23c35745e2566c0
ChristianDonnerborg <S215809@dtu.dk>
2021-10-26 15:10:09 +0200

    DiceCollection Start på kode

--- 10_CDIO-del2/src/Spil/DiceCollection.java
+++ 10_CDIO-del2/src/Spil/DiceCollection.java
@@ -1,4 +1,33 @@
 package Spil;
-//Så skal der laves en DiceCollection
+
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class DiceCollection {
+    private final List dice;
+    public DiceCollection(int sides, int amount) {
+        dice = new ArrayList<Die>();
+        //Creates die with "Sides" untill "amount" of die has been createt.
+        for(int i = 0; i<amount; i++){
+            dice.add(new Die(sides));
+        }
+    }
+    public DiceCollection(int amount) {
+            dice = new ArrayList<Die>();
+            //Creates die with 6 sides untill "amount" of die has been createt.
+            for(int i = 0; i<amount; i++){
+                dice.add(new Die());
+            }
+
+        }
+    public DiceCollection() {
+        dice = new ArrayList<Die>();
+        //Creates die with 6 sides untill 2 die has been createt.
+        for(int i = 0; i<2; i++){
+            dice.add(new Die());
+        }
+
+    }
+
 }
