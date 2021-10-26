a558caa243d0188f6e931f8936a91b7a3db15054
ChristianDonnerborg <S215809@dtu.dk>
2021-10-26 16:11:48 +0200

    DiceCollection return diceArray og kommentar. Die nogle få lavet om fra public til protectet.

--- 10_CDIO-del2/src/Spil/DiceCollection.java
+++ 10_CDIO-del2/src/Spil/DiceCollection.java
@@ -1,33 +1,49 @@
 package Spil;
 
-import java.util.ArrayList;
-import java.util.List;
 
+import java.util.ArrayList;
 
 public class DiceCollection {
-    private final List dice;
+    ArrayList<Die> diceArray = new ArrayList<>();
+
     public DiceCollection(int sides, int amount) {
-        dice = new ArrayList<Die>();
-        //Creates die with "Sides" untill "amount" of die has been createt.
-        for(int i = 0; i<amount; i++){
-            dice.add(new Die(sides));
+        //Creates die with variable "Sides" until variable "amount" of die has been created.
+        for (int i = 0; i < amount; i++) {
+            Die die = new Die(sides);
+            diceArray.add(new Die(sides));
         }
     }
+
     public DiceCollection(int amount) {
-            dice = new ArrayList<Die>();
-            //Creates die with 6 sides untill "amount" of die has been createt.
-            for(int i = 0; i<amount; i++){
-                dice.add(new Die());
-            }
-
-        }
-    public DiceCollection() {
-        dice = new ArrayList<Die>();
-        //Creates die with 6 sides untill 2 die has been createt.
-        for(int i = 0; i<2; i++){
-            dice.add(new Die());
+        /*
+          Creates die with 6 sides until variable "amount" of die has been created.
+          If there has not been set a variable amount of sides. Creates die with 6 sides if nothing else is specified.
+         */
+        for (int i = 0; i < amount; i++) {
+            diceArray.add(new Die());
         }
 
     }
+
+    public DiceCollection() {
+        /*
+         * Creates die with 6 sides until 2 die has been created.
+         * Creates 2 die if nothing else is specified with 6 sides.
+         */
+        for (int i = 0; i < 2; i++) {
+            diceArray.add(new Die());
+        }
+
+    }
+    //Roll of the die
+    public void roll() {
+        diceArray.forEach(Die::rollDie);
+    }
+
+    public ArrayList<Die> getDiceArray() {
+        return diceArray;
+    }
+
 
 }
+
